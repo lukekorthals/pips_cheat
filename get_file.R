701916fe4f19a6e18db3843bc29d832b85764b5e
Luke Korthals <luke-korthals@outlook.de>
2023-02-01 18:15:40 +0100

    testing stuff

---
+++ get_file.R
@@ -0,0 +1,17 @@
+library(knitr)
+library(RCurl)
+
+
+fileurl <- "https://raw.githubusercontent.com/lukekorthals/pips_cheat/main/OpenBook.md"
+knitrRmd <- paste(readLines(textConnection(getURL(fileurl))), collapse="\n")
+
+setwd("C:/Users/luke-/Desktop/Python Repositories/pips_cheat/")
+fn=tempfile()
+fn.Rmd <- paste(fn, ".Rmd", sep="")
+fn.md <- paste(fn, ".md", sep="")
+fn.html <- paste(fn, "-out.html", sep="") 
+## Write R Markdown into a file
+cat(knitrRmd, file=fn.Rmd)
+render_markdown()
+knit(fn.Rmd, fn.md)
+knit2html(fn.md)
